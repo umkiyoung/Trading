03007eca13e2ab0bad0bbeed1abff31905561366
umkiyoung <se99an@gmail.com>
2023-04-30 16:19:32 +0900

    4_30_update

--- ky_/Iterative_SMABOL.py
+++ ky_/Iterative_SMABOL.py
@@ -1,20 +1,18 @@
-
-from IterativeBase import *
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("seaborn")
+from Iterative_Backtester import IterativeBase
 
 class IterativeBacktest(IterativeBase):
-    ''' Class for iterative (event-driven) backtesting of trading strategies.
-    '''
-
-    # helper method
     def go_long(self, bar, units = None, amount = None):
         if units:
             self.buy_instrument(bar, units = units)
         elif amount:
             if amount == "all":
                 amount = self.current_balance
-            self.buy_instrument(bar, amount = amount) # go long
-
-    # helper method
+            self.buy_instrument(bar, amount = amount)
+            
     def go_short(self, bar, units = None, amount = None):
         if units:
             self.sell_instrument(bar, units = units)
@@ -22,8 +20,8 @@ class IterativeBacktest(IterativeBase):
             if amount == "all":
                 amount = self.current_balance
             self.sell_instrument(bar, amount = amount) # go short
-
-    def test_sma_strategy(self, SMA_S, SMA_L):
+            
+    def SMA_BOL(self, SMA_S, SMA_M, SMA_L, dev):
         ''' 
         Backtests an SMA crossover strategy with SMA_S (short) and SMA_L (long).
         
@@ -34,75 +32,58 @@ class IterativeBacktest(IterativeBase):
         SMA_L: int
             moving window in bars (e.g. days) for longer SMA
         '''
-        
-        # nice printout
-        stm = "Testing SMA strategy | {} | SMA_S = {} & SMA_L = {}".format(self.symbol, SMA_S, SMA_L)
+        stm = "Testing SMA With Bolinger strategy | {} | SMA_S = {} & SMA_M = {} & SMA_L = {} DEV = {}".format(self.symbol, SMA_S, SMA_M, SMA_L, dev)
         print("-" * 75)
         print(stm)
         print("-" * 75)
         
-        # reset 
         self.position = 0  # initial neutral position
         self.trades = 0  # no trades yet
         self.current_balance = self.initial_balance  # reset initial capital
         self.get_data() # reset dataset
         
-        # prepare data
-        self.data["SMA_S"] = self.data["price"].rolling(SMA_S).mean()
-        self.data["SMA_L"] = self.data["price"].rolling(SMA_L).mean()
-        self.data.dropna(inplace = True)
+        self.data["SMA_S"] = self.data["Close"].rolling(SMA_S).mean()
+        self.data["SMA_L"] = self.data["Close"].rolling(SMA_L).mean()
+        
+        self.data["SMA"] = self.data["Close"].rolling(SMA_M).mean()
+        self.data["Lower_1"] = self.data["SMA"] - self.data["Close"].rolling(SMA_M).std() * (dev/2)
+        self.data["Upper_1"] = self.data["SMA"] + self.data["Close"].rolling(SMA_M).std() * (dev/2)
+        self.data["Lower_2"] = self.data["SMA"] - self.data["Close"].rolling(SMA_M).std() * dev
+        self.data["Upper_2"] = self.data["SMA"] + self.data["Close"].rolling(SMA_M).std() * dev     
 
-        # sma crossover strategy
+        self.data.dropna(inplace = True)
+        
         for bar in range(len(self.data)-1): # all bars (except the last bar)
+            trade_flag = self.trades
             if self.data["SMA_S"].iloc[bar] > self.data["SMA_L"].iloc[bar]: # signal to go long
-                if self.position in [0, -1]:
-                    self.go_long(bar, amount = "all") # go long with full amount
+                if self.position in [0,-1]:
                     self.position = 1  # long position
             elif self.data["SMA_S"].iloc[bar] < self.data["SMA_L"].iloc[bar]: # signal to go short
                 if self.position in [0, 1]:
-                    self.go_short(bar, amount = "all") # go short with full amount
                     self.position = -1 # short position
+            else:
+                if self.position in [1,-1]:
+                    self.position = 0 #neutral position
+            if self.position == 1 and self.data['Close'].iloc[bar]<self.data['Lower_2'].iloc[bar]:
+                self.go_long(bar, amount='all')
+            elif self.position == 1 and self.data['Close'].iloc[bar]<self.data['Lower_1'].iloc[bar]:
+                self.go_long(bar, amount=min(self.current_balance/10,0))
+            if self.position == -1 and self.data['Close'].iloc[bar]<self.data['Upper_2'].iloc[bar]:
+                self.go_short(bar, amount='all')
+            elif self.position == -1 and self.data['Close'].iloc[bar]<self.data['Upper_1'].iloc[bar]:
+                self.go_long(bar, amount=min(self.current_balance/10,0))
+            if self.trades != trade_flag:
+                self.print_current_nav(bar)
         self.close_pos(bar+1) # close position at the last bar
         
+    def plot_bol(self):
+        plot_list = list(self.data.columns)
+        plot_list.remove('returns')
+        for cols in plot_list:
+            self.data[cols].plot(figsize = (12, 8), title = self.symbol)
+            plt.legend()
         
-    def test_con_strategy(self, window = 1):
-        ''' 
-        Backtests a simple contrarian strategy.
-        
-        Parameters
-        ----------
-        window: int
-            time window (number of bars) to be considered for the strategy.
-        '''
-        
-        # nice printout
-        stm = "Testing Contrarian strategy | {} | Window = {}".format(self.symbol, window)
-        print("-" * 75)
-        print(stm)
-        print("-" * 75)
-        
-        # reset 
-        self.position = 0  # initial neutral position
-        self.trades = 0  # no trades yet
-        self.current_balance = self.initial_balance  # reset initial capital
-        self.get_data() # reset dataset
-        
-        # prepare data
-        self.data["rolling_returns"] = self.data["returns"].rolling(window).mean()
-        self.data.dropna(inplace = True)
-        
-        # Contrarian strategy
-        for bar in range(len(self.data)-1): # all bars (except the last bar)
-            if self.data["rolling_returns"].iloc[bar] <= 0: #signal to go long
-                if self.position in [0, -1]:
-                    self.go_long(bar, amount = "all") # go long with full amount
-                    self.position = 1  # long position
-            elif self.data["rolling_returns"].iloc[bar] > 0: #signal to go short
-                if self.position in [0, 1]:
-                    self.go_short(bar, amount = "all") # go short with full amount
-                    self.position = -1 # short position
-        self.close_pos(bar+1) # close position at the last bar
-        
+   
         
     def test_boll_strategy(self, SMA, dev):
         ''' 
@@ -129,35 +110,34 @@ class IterativeBacktest(IterativeBase):
         self.get_data() # reset dataset
         
         # prepare data
-        self.data["SMA"] = self.data["price"].rolling(SMA).mean()
-        self.data["Lower"] = self.data["SMA"] - self.data["price"].rolling(SMA).std() * dev
-        self.data["Upper"] = self.data["SMA"] + self.data["price"].rolling(SMA).std() * dev
+        self.data["SMA"] = self.data["Close"].rolling(SMA).mean()
+        self.data["Lower"] = self.data["SMA"] - self.data["Close"].rolling(SMA).std() * dev
+        self.data["Upper"] = self.data["SMA"] + self.data["Close"].rolling(SMA).std() * dev
         self.data.dropna(inplace = True) 
         
         # Bollinger strategy
         for bar in range(len(self.data)-1): # all bars (except the last bar)
             if self.position == 0: # when neutral
-                if self.data["price"].iloc[bar] < self.data["Lower"].iloc[bar]: # signal to go long
+                if self.data["Close"].iloc[bar] < self.data["Lower"].iloc[bar]: # signal to go long
                     self.go_long(bar, amount = "all") # go long with full amount
                     self.position = 1  # long position
-                elif self.data["price"].iloc[bar] > self.data["Upper"].iloc[bar]: # signal to go Short
+                elif self.data["Close"].iloc[bar] > self.data["Upper"].iloc[bar]: # signal to go Short
                     self.go_short(bar, amount = "all") # go short with full amount
                     self.position = -1 # short position
             elif self.position == 1: # when long
-                if self.data["price"].iloc[bar] > self.data["SMA"].iloc[bar]:
-                    if self.data["price"].iloc[bar] > self.data["Upper"].iloc[bar]: # signal to go short
+                if self.data["Close"].iloc[bar] > self.data["SMA"].iloc[bar]:
+                    if self.data["Close"].iloc[bar] > self.data["Upper"].iloc[bar]: # signal to go short
                         self.go_short(bar, amount = "all") # go short with full amount
                         self.position = -1 # short position
                     else:
                         self.sell_instrument(bar, units = self.units) # go neutral
                         self.position = 0
             elif self.position == -1: # when short
-                if self.data["price"].iloc[bar] < self.data["SMA"].iloc[bar]:
-                    if self.data["price"].iloc[bar] < self.data["Lower"].iloc[bar]: # signal to go long
+                if self.data["Close"].iloc[bar] < self.data["SMA"].iloc[bar]:
+                    if self.data["Close"].iloc[bar] < self.data["Lower"].iloc[bar]: # signal to go long
                         self.go_long(bar, amount = "all") # go long with full amount
                         self.position = 1 # long position
                     else:
                         self.buy_instrument(bar, units = -self.units) # go neutral
                         self.position = 0                
         self.close_pos(bar+1) # close position at the last bar
-     
